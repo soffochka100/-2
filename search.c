89daf55d35544f08c896cc1af1924f1cd860105d
soffochka100 <148487087+soffochka100@users.noreply.github.com>
2023-12-09 20:56:32 +0300

    Update search.c

--- search.c
+++ search.c
@@ -3,61 +3,64 @@
 #include <stdlib.h>
 #include <string.h>
 
-void search(catcafe *arr, int n){
+void search(catcafe *arr, int n)
+{
 	int key;
 	printf("Поиск:\n 1.Название\n 2.Рейтинг\n 3.Цена за час\n");
 	scanf("%d", &key);
-	switch(key){
+	switch(key)
+	{
 		case 1:{
-			char name_key[40];
-			int count = 0;
-			printf("Введите название: ");
-			scanf("%s",name_key);
-			for(int i = 0; i<n; i++){
-				if(!strcmp(arr[i].name,name_key)){
+			char key1[40];
+			int found = 0;
+			printf("Совпадение найдено: ");
+			scanf("%s",key1);
+			for(int i = 0; i<n; i++)
+			{
+				if(!strcmp(arr[i].name,key1))
+				{
 					printf("Совпадение найдено:\n Название: %s\n Рейтинг: %d\n Цена за час: %d\n", arr[i].name, arr[i].rating, arr[i].price);
-					count = 1;
+					found = 1;
 				}
+
 			}
-			if (!count){
-				printf("Совпадений не найдено\n");
-				}
-			break;
-			}
-		case 2:{
-			int rating_key;
-			int count = 0;
-			printf("Введите рейтинг: ");
-			scanf("%d",&rating_key);
-			for(int i = 0; i<n; i++){
-				if(arr[i].rating == rating_key){
-					printf("Совпадение найдено:\n Название: %s\n Рейтинг: %d\n Цена за час: %d\n", arr[i].name, arr[i].rating, arr[i].price);
-					count = 1;
-				}
-			}
-			if (!count){ 
-				printf("Совпадений не найдено\n");
-				}
-			break;
-			}
-		case 3:{
-			int price_key;
-			int count = 0;
-			printf("Введите цену за час: ");
-			scanf("%d",&price_key);
-			for(int i = 0; i<n; i++){
-				if(arr[i].price == price_key){
-					printf("Совпадение найдено:\n Название: %s\n Рейтинг: %d\n Цена за час: %d\n", arr[i].name, arr[i].rating, arr[i].price);
-					count = 1;
-				}
-			}
-			if (!count){ 
-				printf("Совпадений не найдено\n");
-				}
-			break;
-			}
-		default:
+			if (!found) printf("Совпадений не найдено\n");
 			break;
 		}
+		case 2:{
+			double key2;
+			int found = 0;
+			printf("Введите рейтинг: ");
+			scanf("%lf",&key2);
+			for(int i = 0; i<n; i++)
+			{
+				if(arr[i].rating == key2)
+				{
+					printf("Совпадение найдено:\n Название: %s\n Р Рейтинг: %d\n Цена за час: %d\n", arr[i].name, arr[i].rating, arr[i].price);
+					found = 1;
+				}
+			}
+			if (!found) printf("Совпадений не найдено\n");
+			break;
+		}
+		case 3:{
+			double key3;
+			int found = 0;
+			printf("Введите за час: ");
+			scanf("%lf",&key3);
+			for(int i = 0; i<n; i++)
+			{
+				if(arr[i].price == key3)
+				{
+					printf("Совпадение найдено:\n Название: %s\n Рейтинг: %d\n Цена за час: %d\n", arr[i].name, arr[i].rating, arr[i].price);
+					found = 1;
+				}
+			}
+			if (!found) printf("Совпадений не найдено\n");
+			break;
+		}
+		default:
+			break;
+	}
 	printf("\n");
 }
