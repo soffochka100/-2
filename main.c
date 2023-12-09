ededabca6a8b654729b7bdcb70690be0d24c258c
soffochka100 <148487087+soffochka100@users.noreply.github.com>
2023-12-09 20:30:23 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,33 +7,32 @@ int main(){
 	int stop = 0;
 	while(stop != 1){
 		int keybd;
-		printf("1 - Загрузить бд, 2 - Вывести бд, 3 - Поиск \n");
+		printf("1 - Р—Р°РіСЂСѓР·РёС‚СЊ Р±Рґ, 2 - Р’С‹РІРµСЃС‚Рё Р±Рґ, 3 - РџРѕРёСЃРє \n");
 		scanf("%d", &keybd);
 		switch(keybd){
 			case 1:{
 				int keybd2;
-				printf("1 - Создать, 2 - Прочитать\n");
+				printf("1 - РЎРѕР·РґР°С‚СЊ, 2 - РџСЂРѕС‡РёС‚Р°С‚СЊ\n");
 				scanf("%d", &keybd2);
 			if(keybd2 == 1){
 				create(&arr, &n);
 				if(!arr)
-					printf("Ошибка создания \n");
+					printf("РћС€РёР±РєР° СЃРѕР·РґР°РЅРёСЏ \n");
 				else
-					printf("Созданно %d элементов \n", n);
+					printf("РЎРѕР·РґР°РЅРЅРѕ %d СЌР»РµРјРµРЅС‚РѕРІ \n", n);
 				write(arr, n);
 			}
 			else{
 				read(&arr, &n);
 				if(!arr)
-					printf("Ошибка чтения \n");
+					printf("РћС€РёР±РєР° С‡С‚РµРЅРёСЏ \n");
 				else
-					printf("Загруженно %d элементов \n", n);
+					printf("Р—Р°РіСЂСѓР¶РµРЅРЅРѕ %d СЌР»РµРјРµРЅС‚РѕРІ \n", n);
 			}
 				break;
 			}
 			case 2:{
-				for(int i = 0; i < n; i++)
-					printf("Название: %s\n Рейтинг: %d\n Цена за час: %d \n", arr[i].name, arr[i].rating, arr[i].price);
+				print(arr, n);
 				break;
 			}
 			case 3:	
